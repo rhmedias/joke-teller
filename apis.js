07901799948682fcc427a2cb4343192ad5c6637c
rhmedias <rhmedias@users.noreply.github.com>
2022-01-19 17:29:22 +0100

    Delete apis.js

--- apis.js
+++
@@ -1,5 +0,0 @@
-const SPEECH_API_KEY = "ebbc58d6ad0546fd8238f5204398fbe0";
-const JOKE_API_KEY = "c1da975c94msh84d4bffd9d40d57p1c2788jsn6ecc2887a66d";
-
-export default SPEECH_API_KEY;
-export { JOKE_API_KEY };
